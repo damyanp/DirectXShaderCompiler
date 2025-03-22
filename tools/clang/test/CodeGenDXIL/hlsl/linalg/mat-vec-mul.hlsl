4b804c94e5170df79337932f0aa71b82da86d5c0
Damyan Pepper <damyanp@microsoft.com>
2025-03-22 11:05:48 -0700

    Separate linalg tests

---
+++ tools/clang/test/CodeGenDXIL/hlsl/linalg/mat-vec-mul.hlsl
@@ -0,0 +1,21 @@
+// RUN: %dxc -T lib_6_9 %s | FileCheck %s
+
+#include "linalg.h"
+
+ByteAddressBuffer Buf;
+
+
+export float4 Test4(float4 input) {
+  using namespace dx::linalg;
+
+  MatrixRef<DATA_TYPE_FLOAT16, 4, 4, MATRIX_LAYOUT_INFERENCING_OPTIMAL, true>
+      matrix = {Buf, 0, 0};
+
+  // clang-format off
+  // CHECK: call void {{.*__builtin_Mul@.*}}(<4 x float> %{{.+}}, i32 8, i32 0, i32 0, i32 8, i32 4, i32 4, i32 2, i1 zeroext true, i32 0, <4 x float>* nonnull dereferenceable(16) %{{.+}})
+  return Mul<float>(    
+      matrix, InterpretedVector<DATA_TYPE_FLOAT16>(input));
+  // clang-format on
+}
+
+
---
+++ tools/clang/test/CodeGenDXIL/hlsl/linalg/outerproductaccumulate.hlsl
@@ -0,0 +1,17 @@
+// RUN: %dxc -T lib_6_9 %s | FileCheck %s
+
+#include "linalg.h"
+
+RWByteAddressBuffer RWBuf;
+
+export void Test4(vector<half, 128> Input1, vector<half, 256> Input2) {
+  using namespace dx::linalg;
+
+  RWMatrixRef<DATA_TYPE_FLOAT16, 128, 256, MATRIX_LAYOUT_TRAINING_OPTIMAL>
+      matrix = {RWBuf, 0, 0};
+
+  // clang-format off
+  // CHECK: call void {{.*}}__builtin_OuterProductAccumulate@{{.*}}(<128 x float> %Input1, <256 x float> %Input2, i32 0, i32 0, i32 8, i32 3)
+  // clang-format on
+  OuterProductAccumulate(Input1, Input2, matrix);
+}
