205f9e1fea869737a19d5d46c7fe8c488cf4228b
Damyan Pepper <damyanp@microsoft.com>
2025-04-30 08:31:41 -0700

    Download DXC releases

---
+++ tools/clang/test/DxilBackCompat.cmake
@@ -0,0 +1,21 @@
+# HLSL: Tests for DXIL backward compatability
+include(ExternalProject)
+
+function(add_released_dxc name version)
+    ExternalProject_Add(${name}
+        URL https://github.com/microsoft/DirectXShaderCompiler/releases/download/${version}/${name}.zip
+        DOWNLOAD_DIR dxc_releases
+        SOURCE_DIR dxc_releases/${version}
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+    )
+endfunction()
+
+# https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.8.2502/dxc_2025_02_20.zip
+# https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.7.2308/dxc_2023_08_14.zip
+# https://github.com/microsoft/DirectXShaderCompiler/releases/download/v1.6.2112/dxc_2021_12_08.zip
+
+add_released_dxc(dxc_2025_02_20 v1.8.2502)
+add_released_dxc(dxc_2023_08_14 v1.7.2308)
+add_released_dxc(dxc_2021_12_08 v1.6.2112)
