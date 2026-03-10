55606c669dd75f85fd850cc22f163da846f89025
Damyan Pepper <damyanp@microsoft.com>
2026-03-09 17:35:29 -0700

    Test: badly formatted file to trigger code formatter

---
+++ test-format.cpp
@@ -0,0 +1,13 @@
+// Test file with intentionally bad formatting
+#include <stdio.h>
+
+int main() {
+    int x=1;
+    int y =2;
+    if(x==y){
+        printf("equal");
+    }else{
+        printf("not equal");
+    }
+    return 0;
+}
